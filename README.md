301cd66c8bf98661773e05c3cd885d5ebf9c61b8
Nick Plante <votus998@gmail.com>
2015-06-02 11:29:58 -0400

    Non-functional Commit
    Copied iRestore source code to here, made some minor edits to brand it
    as Royal Rebels. Will begin work on this shortly.

---
+++ README.md
@@ -0,0 +1,1 @@
+### https://github.com/triarry/Royal_Rebels
---
+++ src/com/github/triarry/Royal_Rebels/commands/InfoCommand.java
@@ -0,0 +1,32 @@
+package com.github.triarry.Royal_Rebels.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import com.github.triarry.iRestore.iRestore;
+
+public class InfoCommand implements CommandExecutor {
+
+	@SuppressWarnings("unused")
+	private iRestore plugin;
+	  
+	public InfoCommand(iRestore plugin){ 
+		this.plugin = plugin; 
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (label.equalsIgnoreCase("royal_rebels")) {
+			sender.sendMessage(ChatColor.LIGHT_PURPLE + "#####");
+			sender.sendMessage(ChatColor.GREEN + "Currently running iRestore version 1.6.0");
+			sender.sendMessage(ChatColor.GREEN + "Plugin made by triarry");
+			sender.sendMessage(ChatColor.LIGHT_PURPLE + "#####");
+			return true;
+		}
+		
+		else {
+			return true;
+		}
+	}
+}
